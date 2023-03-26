e4c330a0138c70d1b6471b44de6e74bef677e175
CouldBeThis <53547181+CouldBeThis@users.noreply.github.com>
2023-03-26 16:32:07 -0400

    add back (updated) file.. actually the whole directory should be moved to another branch

---
+++ docs/themes-I-don't-like.sh
@@ -0,0 +1,183 @@
+#!/bin/bash
+
+echo "remove the ugly ones"
+
+# mkdir -p zzz__unused
+
+# mv *-agua zzz__unused
+# mv *-agualemon zzz__unused
+# mv *-b6 zzz__unused
+# mv *-beastie zzz__unused
+# mv *-elberg zzz__unused
+# mv *-fbx zzz__unused
+# mv *-g2 zzz__unused
+# mv *-galaxy zzz__unused
+# mv *-gaudy zzz__unused
+# mv *-gelly zzz__unused
+# mv *-gnububble zzz__unused
+# mv *-gorilla zzz__unused
+# mv *-keramik zzz__unused
+# mv *-kindaker zzz__unused
+# mv *-perl zzz__unused
+# mv *-pills zzz__unused
+# mv *-prune zzz__unused
+# mv *-redmondxp zzz__unused
+# mv *-retro zzz__unused
+# mv *-sassandra zzz__unused
+# mv *-silverado zzz__unused
+# mv *-smallscreen zzz__unused
+# mv *-smoothwall zzz__unused
+# mv *-stoneage zzz__unused
+# mv *-symphony zzz__unused
+# mv *-tabs zzz__unused
+# mv *-tgc zzz__unused
+# mv *-therapy zzz__unused
+# mv *-tux zzz__unused
+# mv *-tyrex zzz__unused
+# mv *-wallis zzz__unused
+# mv *-waza zzz__unused
+# mv *-wildbush zzz__unused
+
+
+# fd -i -d 1 "^agua" --exec mv {/} zzz__unused
+# fd -i -d 1 "^agualemon" --exec mv {/} zzz__unused
+# fd -i -d 1 "^b6" --exec mv {/} zzz__unused
+# fd -i -d 1 "^beastie" --exec mv {/} zzz__unused
+# fd -i -d 1 "^elberg" --exec mv {/} zzz__unused
+# fd -i -d 1 "^fbx" --exec mv {/} zzz__unused
+# fd -i -d 1 "^g2" --exec mv {/} zzz__unused
+# fd -i -d 1 "^galaxy" --exec mv {/} zzz__unused
+# fd -i -d 1 "^gaudy" --exec mv {/} zzz__unused
+# fd -i -d 1 "^gelly" --exec mv {/} zzz__unused
+# fd -i -d 1 "^gnububble" --exec mv {/} zzz__unused
+# fd -i -d 1 "^gorilla" --exec mv {/} zzz__unused
+# fd -i -d 1 "^keramik" --exec mv {/} zzz__unused
+# fd -i -d 1 "^kindaker" --exec mv {/} zzz__unused
+# fd -i -d 1 "^perl" --exec mv {/} zzz__unused
+# fd -i -d 1 "^pills" --exec mv {/} zzz__unused
+# fd -i -d 1 "^prune" --exec mv {/} zzz__unused
+# fd -i -d 1 "^redmondxp" --exec mv {/} zzz__unused
+# fd -i -d 1 "^retro" --exec mv {/} zzz__unused
+# fd -i -d 1 "^sassandra" --exec mv {/} zzz__unused
+# fd -i -d 1 "^silverado" --exec mv {/} zzz__unused
+# fd -i -d 1 "^smallscreen" --exec mv {/} zzz__unused
+# fd -i -d 1 "^smoothwall" --exec mv {/} zzz__unused
+# fd -i -d 1 "^stoneage" --exec mv {/} zzz__unused
+# fd -i -d 1 "^symphony" --exec mv {/} zzz__unused
+# fd -i -d 1 "^tabs" --exec mv {/} zzz__unused
+# fd -i -d 1 "^tgc" --exec mv {/} zzz__unused
+# fd -i -d 1 "^therapy" --exec mv {/} zzz__unused
+# fd -i -d 1 "^tux" --exec mv {/} zzz__unused
+# fd -i -d 1 "^tyrex" --exec mv {/} zzz__unused
+# fd -i -d 1 "^wallis" --exec mv {/} zzz__unused
+# fd -i -d 1 "^waza" --exec mv {/} zzz__unused
+# fd -i -d 1 "^wildbush" --exec mv {/} zzz__unused
+
+
+
+# fd -i -d 1 "^agua" --exec mv {/} zzz__unused
+# fd -i -d 1 "^agualemon" --exec mv {/} zzz__unused
+# fd -i -d 1 "^b6" --exec mv {/} zzz__unused
+# fd -i -d 1 "^beastie" --exec mv {/} zzz__unused
+# fd -i -d 1 "^elberg" --exec mv {/} zzz__unused
+# fd -i -d 1 "^fbx" --exec mv {/} zzz__unused
+# fd -i -d 1 "^g2" --exec mv {/} zzz__unused
+# fd -i -d 1 "^galaxy" --exec mv {/} zzz__unused
+# fd -i -d 1 "^gaudy" --exec mv {/} zzz__unused
+# fd -i -d 1 "^gelly" --exec mv {/} zzz__unused
+# fd -i -d 1 "^gnububble" --exec mv {/} zzz__unused
+# fd -i -d 1 "^gorilla" --exec mv {/} zzz__unused
+# fd -i -d 1 "^keramik" --exec mv {/} zzz__unused
+# fd -i -d 1 "^kindaker" --exec mv {/} zzz__unused
+# fd -i -d 1 "^perl" --exec mv {/} zzz__unused
+# fd -i -d 1 "^pills" --exec mv {/} zzz__unused
+# fd -i -d 1 "^prune" --exec mv {/} zzz__unused
+# fd -i -d 1 "^redmondxp" --exec mv {/} zzz__unused
+# fd -i -d 1 "^retro" --exec mv {/} zzz__unused
+# fd -i -d 1 "^sassandra" --exec mv {/} zzz__unused
+# fd -i -d 1 "^silverado" --exec mv {/} zzz__unused
+# fd -i -d 1 "^smallscreen" --exec mv {/} zzz__unused
+# fd -i -d 1 "^smoothwall" --exec mv {/} zzz__unused
+# fd -i -d 1 "^stoneage" --exec mv {/} zzz__unused
+# fd -i -d 1 "^symphony" --exec mv {/} zzz__unused
+# fd -i -d 1 "^tabs" --exec mv {/} zzz__unused
+# fd -i -d 1 "^tgc" --exec mv {/} zzz__unused
+# fd -i -d 1 "^therapy" --exec mv {/} zzz__unused
+# fd -i -d 1 "^tux" --exec mv {/} zzz__unused
+# fd -i -d 1 "^tyrex" --exec mv {/} zzz__unused
+# fd -i -d 1 "^wallis" --exec mv {/} zzz__unused
+# fd -i -d 1 "^waza" --exec mv {/} zzz__unused
+# fd -i -d 1 "^wildbush" --exec mv {/} zzz__unused
+
+
+# fd -i -d 1 "^agua" --exec mv {/} zzz__unused
+# fd -i -d 1 "^agualemon" --exec mv {/} zzz__unused
+# fd -i -d 1 "^b6" --exec mv {/} zzz__unused
+# fd -i -d 1 "^beastie" --exec mv {/} zzz__unused
+# fd -i -d 1 "^elberg" --exec mv {/} zzz__unused
+# fd -i -d 1 "^fbx" --exec mv {/} zzz__unused
+# fd -i -d 1 "^g2" --exec mv {/} zzz__unused
+# fd -i -d 1 "^galaxy" --exec mv {/} zzz__unused
+# fd -i -d 1 "^gaudy" --exec mv {/} zzz__unused
+# fd -i -d 1 "^gelly" --exec mv {/} zzz__unused
+# fd -i -d 1 "^gnububble" --exec mv {/} zzz__unused
+# fd -i -d 1 "^gorilla" --exec mv {/} zzz__unused
+# fd -i -d 1 "^keramik" --exec mv {/} zzz__unused
+# fd -i -d 1 "^kindaker" --exec mv {/} zzz__unused
+# fd -i -d 1 "^perl" --exec mv {/} zzz__unused
+# fd -i -d 1 "^pills" --exec mv {/} zzz__unused
+# fd -i -d 1 "^prune" --exec mv {/} zzz__unused
+# fd -i -d 1 "^redmondxp" --exec mv {/} zzz__unused
+# fd -i -d 1 "^retro" --exec mv {/} zzz__unused
+# fd -i -d 1 "^sassandra" --exec mv {/} zzz__unused
+# fd -i -d 1 "^silverado" --exec mv {/} zzz__unused
+# fd -i -d 1 "^smallscreen" --exec mv {/} zzz__unused
+# fd -i -d 1 "^smoothwall" --exec mv {/} zzz__unused
+# fd -i -d 1 "^stoneage" --exec mv {/} zzz__unused
+# fd -i -d 1 "^symphony" --exec mv {/} zzz__unused
+# fd -i -d 1 "^tabs" --exec mv {/} zzz__unused
+# fd -i -d 1 "^tgc" --exec mv {/} zzz__unused
+# fd -i -d 1 "^therapy" --exec mv {/} zzz__unused
+# fd -i -d 1 "^tux" --exec mv {/} zzz__unused
+# fd -i -d 1 "^tyrex" --exec mv {/} zzz__unused
+# fd -i -d 1 "^wallis" --exec mv {/} zzz__unused
+# fd -i -d 1 "^waza" --exec mv {/} zzz__unused
+# fd -i -d 1 "^wildbush" --exec mv {/} zzz__unused
+
+fd -i -d 1 "^gaudy" --exec sudo mv "{/}" "{/}-7777"
+
+fd -i -d 1 "^agua$" --exec sudo mv "{/}" "{/}-7777"
+
+fd -i -d 1 "^agualemon" --exec sudo mv "{/}" "{/}-7777"
+
+fd -i -d 1 "^b6" --exec sudo mv "{/}" "{/}-7777"
+fd -i -d 1 "^beastie" --exec sudo mv "{/}" "{/}-7777"
+fd -i -d 1 "^elberg" --exec sudo mv "{/}" "{/}-7777"
+fd -i -d 1 "^fbx" --exec sudo mv "{/}" "{/}-7777"
+fd -i -d 1 "^g2" --exec sudo mv "{/}" "{/}-7777"
+fd -i -d 1 "^galaxy" --exec sudo mv "{/}" "{/}-7777"
+fd -i -d 1 "^gaudy" --exec sudo mv "{/}" "{/}-7777"
+fd -i -d 1 "^gelly" --exec sudo mv "{/}" "{/}-7777"
+fd -i -d 1 "^gnububble" --exec sudo mv "{/}" "{/}-7777"
+fd -i -d 1 "^gorilla" --exec sudo mv "{/}" "{/}-7777"
+fd -i -d 1 "^keramik" --exec sudo mv "{/}" "{/}-7777"
+fd -i -d 1 "^kindaker" --exec sudo mv "{/}" "{/}-7777"
+fd -i -d 1 "^perl" --exec sudo mv "{/}" "{/}-7777"
+fd -i -d 1 "^pills" --exec sudo mv "{/}" "{/}-7777"
+fd -i -d 1 "^prune" --exec sudo mv "{/}" "{/}-7777"
+fd -i -d 1 "^redmondxp" --exec sudo mv "{/}" "{/}-7777"
+fd -i -d 1 "^retro" --exec sudo mv "{/}" "{/}-7777"
+fd -i -d 1 "^sassandra" --exec sudo mv "{/}" "{/}-7777"
+fd -i -d 1 "^silverado" --exec sudo mv "{/}" "{/}-7777"
+fd -i -d 1 "^smallscreen" --exec sudo mv "{/}" "{/}-7777"
+fd -i -d 1 "^smoothwall" --exec sudo mv "{/}" "{/}-7777"
+fd -i -d 1 "^stoneage" --exec sudo mv "{/}" "{/}-7777"
+fd -i -d 1 "^symphony" --exec sudo mv "{/}" "{/}-7777"
+fd -i -d 1 "^tabs" --exec sudo mv "{/}" "{/}-7777"
+fd -i -d 1 "^tgc" --exec sudo mv "{/}" "{/}-7777"
+fd -i -d 1 "^therapy" --exec sudo mv "{/}" "{/}-7777"
+fd -i -d 1 "^tux" --exec sudo mv "{/}" "{/}-7777"
+fd -i -d 1 "^tyrex" --exec sudo mv "{/}" "{/}-7777"
+fd -i -d 1 "^wallis" --exec sudo mv "{/}" "{/}-7777"
+fd -i -d 1 "^waza" --exec sudo mv "{/}" "{/}-7777"
+fd -i -d 1 "^wildbush" --exec sudo mv "{/}" "{/}-7777"
