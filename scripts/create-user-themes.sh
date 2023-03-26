7dfb1ffbf337ade83a925615139dfaeaa8f8e58b
CouldBeThis <53547181+CouldBeThis@users.noreply.github.com>
2023-03-26 19:15:45 -0400

    fixed error in sourcing

--- scripts/create-user-themes.sh
+++ scripts/create-user-themes.sh
@@ -5,8 +5,9 @@
 ## Comment out any sizes you don't want
 ## More info: https://github.com/CouldBeThis/xfwm4-themes-HiDPI
 
+# 	echo -e "ThisScriptDirRel is $ThisScriptDirRel"
 ## import delcations (but hide the output)
-source "$ThisScriptDirRel/declarations.sh" > /dev/null
+source "declarations.sh" > /dev/null
 
 echo -e "$c\ee------------------------"
 echo "A very simple script to create theme diretories in your USER folder."
@@ -14,6 +15,13 @@ echo -e "Hit $b\eeenter$bo to continue, $b\eectrl-c$bo to exit"
 read -p "OK?" InputValue
 
 
+
+# themes-100
+# themes-128
+# themes-150
+# themes-175
+# themes-200
+# themes-300
 
 # ls -1 | xargs -I @ ln -s $PWD/@ ~/.themes/200-@
 
