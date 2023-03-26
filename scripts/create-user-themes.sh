834eb825b52340749bd2674396af66331462967a
CouldBeThis <53547181+CouldBeThis@users.noreply.github.com>
2023-03-26 19:37:17 -0400

    clearer way to check that script is being run from correct directory

--- scripts/create-user-themes.sh
+++ scripts/create-user-themes.sh
@@ -7,21 +7,43 @@
 
 # 	echo -e "ThisScriptDirRel is $ThisScriptDirRel"
 ## import delcations (but hide the output)
-source "declarations.sh" > /dev/null
+# source "declarations.sh" > /dev/null
+
+ThisScriptDirRel="$(dirname "$0")"
+# 	echo -e "ThisScriptDirRel is $ThisScriptDirRel"
+## import delcations (but hide the output)
+source "$ThisScriptDirRel/declarations.sh" > /dev/null
 
 echo -e "$c\ee------------------------"
 echo "A very simple script to create theme diretories in your USER folder."
+
+echo -e "CHECK: Is the script being run from the correct driectory?"
+	if [ $ThisScriptDirAbs = "$WorkingDir" ]
+	then
+		echo -e "Yes"
+		export CheckRunDir=Yes
+	else
+		echo -e "$r$b\eeEXITING:$bo please ONLY run this script from the $u\eescripts$uo subdirectory of the repo"
+		echo -e "Your $u\eeworking dir$uo is $b$WorkingDir$bo"
+	echo -e "The $u\eecorrect dir$uo is  $b$ThisScriptDirAbs$bo"
+		export CheckRunDir=No
+		exit
+	fi
+
+echo "$oo$bl^^^^^^^^^^^^^^^^^^^^^^^^^"
+
+
+# 	echo -e "$r$b\eeEXITING:$bo please ONLY run this script from the $u\eescripts$uo subdirectory of the repo. I think it is:
+# 	$b$ThisScriptDirAbs$bo"
+
+
+
 echo -e "Hit $b\eeenter$bo to continue, $b\eectrl-c$bo to exit"
-read -p "OK?" InputValue
+# read -p "OK?" InputValue
 
 
 
-# themes-100
-# themes-128
-# themes-150
-# themes-175
-# themes-200
-# themes-300
+
 
 # ls -1 | xargs -I @ ln -s $PWD/@ ~/.themes/200-@
 
