4d9bfb89e90d8df06a174d65f84cf7b3c5145510
CouldBeThis <53547181+CouldBeThis@users.noreply.github.com>
2023-03-27 00:40:44 -0400

    organize file logically, add silly formatting, describe variables

--- scripts/declarations.sh
+++ scripts/declarations.sh
@@ -13,32 +13,49 @@
 
 ####### text style
 ##	Underline
-	u=$(tput smul)		## on
-	uo=$(tput rmul)		## off
+u=$(tput smul)		## on
+uo=$(tput rmul)		## off
 ## Bold (short)
-	b=$(tput smso)		## on
-	bo=$(tput rmso)		## off
+b=$(tput smso)		## on
+bo=$(tput rmso)		## off
 ## set foreground color
-	bk=$(tput setaf 0)	## $bk
-	r=$(tput setaf 1)	## $r
-	g=$(tput setaf 2)	## $g
-	y=$(tput setaf 3)	## $y
-	bl=$(tput setaf 4)	## $bl
-	m=$(tput setaf 5)	## $m
-	c=$(tput setaf 6)	## $c
-	w=$(tput setaf 7)	## $w
-	oo=$(tput sgr0)		## $oo	## reset all attributes
-echo -e "$r	~~~~~~ DECLARATIONS - variables"
+bk=$(tput setaf 0)	## $bk
+r=$(tput setaf 1)	## $r
+g=$(tput setaf 2)	## $g
+y=$(tput setaf 3)	## $y
+bl=$(tput setaf 4)	## $bl
+m=$(tput setaf 5)	## $m
+c=$(tput setaf 6)	## $c
+w=$(tput setaf 7)	## $w
+oo=$(tput sgr0)		## $oo	## reset all attributes
+
+	echo -e "$r	~~~~~~ DECLARATIONS"
+
+tabs -6 ##default
+	echo -e "$u\eetabs$uo:	8	▇	▓	▒	░	"
 
 #######  variable declarations
 ### Date stamps
 DateTimeStamp=$(date +"%y%m%d-%H%M%S")
-echo -e "$u\eeDateTimeStamp$uo is $b$DateTimeStamp$bo"
+	echo -e "$u\eeDateTimeStamp$uo:	$b$DateTimeStamp$bo"
 DateStamp=$(date +"%y%m%d")
-echo -e "$u\eeDateStamp$uo is $b$DateStamp$bo"
+	echo -e "$u\eeDateStamp$uo:		$b$DateStamp$bo"
 DatePlain=$(date)
-echo -e "$u\eeDatePlain$uo is $b$DatePlain$bo"
+	echo -e "$u\eeDatePlain$uo:		$b$DatePlain$bo"
+
+### environment
+CurrentUser=$(whoami)
+	echo -e "$u\eeCurrentUser$uo:	$b$CurrentUser$bo"
+
 ### Metadata - Script
+
+echo -e "
+$oo\eeInformation about the script:$r"
+
+ThisScript=$(basename "$0")
+	echo -e "$u\eeThisScript$uo:		$b$ThisScript$bo
+	↳ name of script file"
+
 ## These values may be useful when script is run
 ## outside of its original directory, particularly
 ## if it references (includes, sources) other files
@@ -50,20 +67,31 @@ echo -e "$u\eeDatePlain$uo is $b$DatePlain$bo"
 ## 	- Might actually cause problems to declare in here? Try
 ##	commenting out; especially if anything else is sourced after
 ## 	this file
-ThisScriptDirRel="$(dirname "$0")"
-	echo -e "$u\eeThisScriptDirRel$uo is $b$ThisScriptDirRel$bo"
+
 ThisScriptDirAbs=$(dirname $(readlink -f $0))
-	echo -e "$u\eeThisScriptDirAbs$uo is $b$ThisScriptDirAbs$bo"
+	echo -e "$u\eeThisScriptDirAbs$uo:	$b$ThisScriptDirAbs$bo
+	↳ complete path to directory containing the script file"
 
-###	Metadata
-	WorkingDir=$PWD
-	echo -e "$u\eeWorkingDir$uo is $b$WorkingDir$bo"
+echo -e "
+$oo\eeVariables that reflect current working directory of user:$r"
+
+ThisScriptDirRel="$(dirname "$0")"
+	echo -e "$u\eeThisScriptDirRel$uo:	$b$ThisScriptDirRel$bo
+	↳ path to directory containing the script, relative to CWD"
+
+
+WorkingDir=$PWD
+	echo -e "$u\eeWorkingDir$uo:		$b$WorkingDir$bo
+	↳ CWD (where script is running)"
+
 WorkingDirUp1=$(cd .. && pwd)
-echo -e "$u\eeWorkingDirUp1$uo is $b$WorkingDirUp1$bo"
-ThisScript=$(basename "$0")
-	echo -e "$u\eeThisScript$uo is $b$ThisScript$bo"
-CurrentUser=$(whoami)
-	echo -e "$u\eeCurrentUser$uo is $b$CurrentUser$bo"
+	echo -e "$u\eeWorkingDirUp1$uo:	$b$WorkingDirUp1$bo
+	↳ 1 level up from CWD"
 
 
-echo "	~~~~~~ end$oo"
+WorkingDirUp2=$(cd ../.. && pwd)
+	echo -e "$u\eeWorkingDirUp2$uo:	$b$WorkingDirUp2$bo
+	↳ 2 level up from CWD"
+
+
+echo "~~~~~~ end$oo"
